b50d393ccd113a8bad4027bc6ba5b4999449d985
João Pedro Moreno <devmoreno2003@gmail.com>
2023-05-15 15:04:37 -0300

    Transactions table created

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
-import { Header } from './components/Header'
-import { Summary } from './components/Summary'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
